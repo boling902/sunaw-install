ee1316b7550607d8be92c859f66b7a4ae059ded3
abo <abowu@qq.com>
2016-05-13 18:53:27 +0800

    更新

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,6 +13,7 @@ class Plugin implements PluginInterface
     public function activate(Composer $composer, IOInterface $io)
     {
 		echo 'iiii';
+		echo '更新';
         $installer = new SunawFramework($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
